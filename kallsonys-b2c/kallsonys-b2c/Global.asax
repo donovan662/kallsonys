346fd61bd027d39697fb2d37709ce7e68f7916a7
Donovan <DONOVAN_662@hotmail.com>
2016-02-21 11:31:43 -0500

    First commit

---
+++ kallsonys-b2c/kallsonys-b2c/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="kallsonys_b2c.MvcApplication" Language="C#" %>
---
+++ kallsonys-b2c/kallsonys-b2c/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(kallsonys_b2c.Startup))]
+namespace kallsonys_b2c
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
